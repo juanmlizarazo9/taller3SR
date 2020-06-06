3af7acf8c70733198ab5712daac4272c2dbf03b3
juanmlizarazo9 <jm.lizarazo10@uniandes.edu.co>
2020-06-06 16:30:29 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/resources/templates/taller3.ftl

---
+++ src/main/java/com/sr/Models/User.java
@@ -0,0 +1,12 @@
+package com.sr.Models;
+
+public class User {
+    private String id;
+    public User(String id){
+        this.id = id;
+    }
+
+    public String darId(){
+        return id;
+    }
+}
